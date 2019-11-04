8bee67c973f83510a048deb6c878db4979a82732
yoga setiawan <yogainformatika@gmail.com>
2019-11-04 21:35:57 +0700

    penerapan anotasi @Parcelize

--- app/src/main/java/com/arioki/submission1/data/FootballItem.kt
+++ app/src/main/java/com/arioki/submission1/data/FootballItem.kt
@@ -1,35 +1,12 @@
 package com.arioki.submission1.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class FootballItem(
     val id: String?,
     val name: String?,
     val badge: Int,
     val description: String?
-) : Parcelable {
-    constructor(source: Parcel) : this(
-        source.readString(),
-        source.readString(),
-        source.readInt(),
-        source.readString()
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-        writeString(id)
-        writeString(name)
-        writeInt(badge)
-        writeString(description)
-    }
-
-    companion object {
-        @JvmField
-        val CREATOR: Parcelable.Creator<FootballItem> = object : Parcelable.Creator<FootballItem> {
-            override fun createFromParcel(source: Parcel): FootballItem = FootballItem(source)
-            override fun newArray(size: Int): Array<FootballItem?> = arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable

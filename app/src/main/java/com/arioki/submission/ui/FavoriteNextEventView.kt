521c91a452836edd576d7f62e3cc04d8be230ff1
yoga setiawan <yogainformatika@gmail.com>
2019-12-11 22:42:40 +0700

    clear file

--- app/src/main/java/com/arioki/submission/ui/FavoriteNextEventView.kt
+++
@@ -1,7 +0,0 @@
-package com.arioki.submission.ui
-
-import com.arioki.submission.data.EventItem
-
-interface FavoriteNextEventView {
-    fun getDataDone(result: List<EventItem>)
-}
--- app/src/main/java/com/arioki/submission/ui/FavoritePastEventView.kt
+++
@@ -1,8 +0,0 @@
-package com.arioki.submission.ui
-
-import com.arioki.submission.data.EventItem
-
-interface FavoritePastEventView {
-    fun getDataDone(result: List<EventItem>)
-
-}
